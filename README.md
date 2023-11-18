c4e51cf7abd1fa344b3caeb44d32b9fcc1f710fb
Valeriya Kitova <kitova.lerra@yandex.ru>
2023-11-18 17:57:14 +0300

    add tests

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# qa_python
+Список тестов в проекте: 
+1. test_add_new_book_not_add_book_name_with_len_more_40_simbols - проверяет, что в словарь books_genre нельзя добавить книгу с кол-вом символов в названии > 40
+2. test_set_book_genre_set_genre_for_book_not_in_dict - проверяет, что нельзя установить жанр для книги, которой нет в словаре books_genre
+3. test_set_book_genre_set_genre_for_book_in_dict - проверяет, что можно установить жанр для книги, которая есть в словарь books_genre
+4. test_get_book_genre_for_book_not_in_dict - проверяет, что нельзя получить жанр книги6 которой нет в словаре books_genre
+5. test_get_books_with_specific_genre_for_unavailable_genre - проверяет, что нельзя вывести список книг с определённым жанром, если жанра нет в списке genre
+6. test_get_books_with_specific_genre_for_available_genre - проверяет, что можно вывести список книг с определённым жанром, если этот жанр есть в списке genre
+7. test_get_books_genre_return_dict - проверяем, что метод возвращает словарь
+8. test_get_books_for_children_return_right_books - проверяем, что метод возвращает книги, подходящие детям
+9. test_get_books_for_children_not_return_wrong_books - проверяем, что метод не возвращает книги, подходящие детям
+10. test_add_book_in_favorites_is_successful - проверяем, что метод позволяет добавить книги в список favorites
+11. test_add_book_in_favorites_add_two_books - проверяем, что метод позволяет добавить две книги в список favorites
+12. test_add_book_in_favorites_twice_is_unsuccessful - проверяем, что нельзя добавить в список favorites книгу с одним и тем же названием дважды
+13. test_delete_book_from_favorites_is_successful - проверяем, что можно удалить книгу из списка favorites
+14. test_delete_book_from_favorites_delete_two_books - проверяем, что можно удалить две книги из списка favorites
+15. test_get_list_of_favorites_books_return_list - проверяем, что метод get_list_of_favorites_books возвращает список
--- tests.py
+++ tests.py
@@ -1,7 +1,12 @@
+import pytest
+
 from main import BooksCollector
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
+
+
 class TestBooksCollector:
 
     # пример теста:
@@ -18,7 +23,94 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_not_add_book_name_with_len_more_40_simbols(self):
+        collector = BooksCollector()
+        collector.add_new_book('Преступление и наказание в бандитском Петербурге')
+        assert 'Преступление и наказание в бандитском Петербурге' not in collector.get_books_genre()
+
+    def test_set_book_genre_set_genre_for_book_not_in_dict(self):
+        collector = BooksCollector()
+        collector.set_book_genre('Форест Гамп', 'Фантастика')
+        assert collector.get_book_genre('Форест Гамп') is None
+
+    def test_set_book_genre_set_genre_for_book_in_dict(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.set_book_genre('Форест Гамп', 'Фантастика')
+        assert collector.get_book_genre('Форест Гамп') == 'Фантастика'
+
+    def test_get_book_genre_for_book_not_in_dict(self):
+        collector = BooksCollector()
+        collector.get_book_genre('Форест Гамп')
+        assert collector.get_book_genre('Форест Гамп') is None
+
+    def test_get_books_with_specific_genre_for_unavailable_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.set_book_genre('Форест Гамп', 'Мистика')
+        assert len(collector.get_books_with_specific_genre('Мистика')) == 0
+
+    def test_get_books_with_specific_genre_for_available_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.set_book_genre('Форест Гамп', 'Фантастика')
+        assert len(collector.get_books_with_specific_genre('Фантастика')) == 1
+
+    def test_get_books_genre_return_dict(self):
+        collector = BooksCollector()
+        collector.get_books_genre()
+        assert collector.get_books_genre() == dict()
+
+    def test_get_books_for_children_return_right_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.set_book_genre('Форест Гамп', 'Фантастика')
+        assert len(collector.get_books_for_children()) == 1
+
+    def test_get_books_for_children_not_return_wrong_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.set_book_genre('Форест Гамп', 'Ужасы')
+        assert 'Ужасы' not in collector.get_books_for_children()
+
+    def test_add_book_in_favorites_is_successful(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.add_book_in_favorites('Форест Гамп')
+        assert 'Форест Гамп' in collector.get_list_of_favorites_books()
+
+    @pytest.mark.parametrize('book_name', ['Форест Гамп', 'Гарри Поттер'])
+    def test_add_book_in_favorites_add_two_books(self, book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.add_book_in_favorites(book_name)
+        assert book_name in collector.get_list_of_favorites_books()
+
+    def test_add_book_in_favorites_twice_is_unsuccessful(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.add_book_in_favorites('Форест Гамп')
+        collector.add_book_in_favorites('Форест Гамп')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites_is_successful(self):
+        collector = BooksCollector()
+        collector.add_new_book('Форест Гамп')
+        collector.delete_book_from_favorites('Форест Гамп')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    @pytest.mark.parametrize('book_name', ['Форест Гамп', 'Гарри Поттер'])
+    def test_delete_book_from_favorites_delete_two_books(self, book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.delete_book_from_favorites(book_name)
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_get_list_of_favorites_books_return_list(self):
+        collector = BooksCollector()
+        assert collector.get_list_of_favorites_books() == list()
